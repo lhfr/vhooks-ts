78bd9fe2eeee5aaee4ed3df47bbe44dfe88f9228
white666 <8967008+whites666@user.noreply.gitee.com>
2024-06-21 10:45:00 +0800

    ts

---
+++ packages/tsup.config.ts
@@ -0,0 +1,13 @@
+import { defineConfig } from 'tsup'
+
+export default defineConfig({
+  entry: ['src/index.ts'],
+  dts: true,
+  outDir: 'dist',
+  clean: true,
+  format: ['cjs', 'esm'],
+  treeshake: true,
+  splitting: false,
+  cjsInterop: true,
+  external: ['vue']
+})
