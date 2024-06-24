3a797bc43159f476259c19f4533a364c5898a25a
white666 <8967008+whites666@user.noreply.gitee.com>
2024-06-24 13:58:06 +0800

    useBoolean

---
+++ packages/src/useBoolean.ts
@@ -0,0 +1,27 @@
+import { ref, Ref } from 'vue'
+
+type UseBooleanReturn = {
+    value: Ref<boolean>
+    setValue: (value: boolean) => void
+    setTrue: () => void
+    setFalse: () => void
+    toggle: () => void
+}
+
+export function useBoolean(defaultValue = false): UseBooleanReturn {
+    if (typeof defaultValue !== 'boolean') {
+        throw new Error('defaultValue must be `true` or `false`')
+    }
+    const value = ref(defaultValue)
+    const setValue = (x: boolean | ((x: boolean) => boolean)) => {
+        value.value = typeof x === 'boolean' ? x : x(value.value)
+    }
+
+    const setTrue = () => setValue(true)
+
+    const setFalse = () => setValue(false)
+
+    const toggle = () => setValue(x => !x)
+
+    return { value, setValue, setTrue, setFalse, toggle }
+}
