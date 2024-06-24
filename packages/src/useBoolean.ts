dbbb0176441f30355357ff3bc4c725a3d90a03f0
white666 <8967008+whites666@user.noreply.gitee.com>
2024-06-24 14:17:19 +0800

    useEventListener

--- packages/src/useBoolean.ts
+++ packages/src/useBoolean.ts
@@ -1,27 +1,27 @@
-import { ref, Ref } from 'vue'
+import { ref, Ref } from "vue";
 
 type UseBooleanReturn = {
-    value: Ref<boolean>
-    setValue: (value: boolean) => void
-    setTrue: () => void
-    setFalse: () => void
-    toggle: () => void
-}
+  value: Ref<boolean>;
+  setValue: (value: boolean) => void;
+  setTrue: () => void;
+  setFalse: () => void;
+  toggle: () => void;
+};
 
 export function useBoolean(defaultValue = false): UseBooleanReturn {
-    if (typeof defaultValue !== 'boolean') {
-        throw new Error('defaultValue must be `true` or `false`')
-    }
-    const value = ref(defaultValue)
-    const setValue = (x: boolean | ((x: boolean) => boolean)) => {
-        value.value = typeof x === 'boolean' ? x : x(value.value)
-    }
+  if (typeof defaultValue !== "boolean") {
+    throw new Error("defaultValue must be `true` or `false`");
+  }
+  const value = ref(defaultValue);
+  const setValue = (x: boolean | ((x: boolean) => boolean)) => {
+    value.value = typeof x === "boolean" ? x : x(value.value);
+  };
 
-    const setTrue = () => setValue(true)
+  const setTrue = () => setValue(true);
 
-    const setFalse = () => setValue(false)
+  const setFalse = () => setValue(false);
 
-    const toggle = () => setValue(x => !x)
+  const toggle = () => setValue((x) => !x);
 
-    return { value, setValue, setTrue, setFalse, toggle }
+  return { value, setValue, setTrue, setFalse, toggle };
 }
